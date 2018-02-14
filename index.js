8c0c6f571916dbd578c6dbca093f00b1ce4f18b2
parth-discus-solutions <34234203+parth-discus-solutions@users.noreply.github.com>
2018-02-14 15:54:15 +0530

    567

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-<<<<<<< HEAD
-  response.send('Hello World! test321')
-=======
-  response.send('Hello World! test 123')
->>>>>>> 63455d412fe54e7b7f7d907a6259d4dc90d014bf
-})
+  response.send('Hello World! test   567')
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
