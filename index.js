fff5633db7b0c9671d06b356fd37dbc302c5190c
parth-techify <34234203+parth-techify@users.noreply.github.com>
2018-02-17 23:33:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World! test 11')
+  response.send('Hello World! test 12')
 });
 
 app.listen(app.get('port'), function() {
